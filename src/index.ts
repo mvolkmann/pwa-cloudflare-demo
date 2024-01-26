06b79db0d66b42c879609cc3f8c1bba0a82b9679
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-26 12:58:40 -0600

    progress

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
+
+// Serve static files from the public directory.
+app.use("/*", serveStatic({ root: "./public" }));
 
 app.get("/demo", (c) => {
   return c.html("Hello Hono!");
