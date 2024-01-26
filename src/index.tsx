b7268eba7c711c39bb09f36e2c92ed21a497fed6
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-26 14:56:55 -0600

    progress

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,12 @@ const app = new Hono();
 // Serve static files from the public directory.
 app.use("/*", serveStatic({ root: "./" }));
 
-app.get("/demo", (c) => {
-  // TODO: Try using JSX and c.html.
-  return c.text("htmx works!");
+app.get("/get-text", (c) => {
+  return c.text("This is text.");
+});
+
+app.get("/get-html", (c) => {
+  return c.html(<b>This is HTML.</b>);
 });
 
 export default app;
