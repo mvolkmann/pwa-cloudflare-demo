622f82a63b9799d30c103062454f61128e66d5b1
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-01 16:48:24 -0600

    progress

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export * from './idb-easy.js';
+export * from './dog-controller.js';
 export * from './dog-router.js';
+export * from './idb-easy.js';
 export * from './js2html.js';
 export * from './service-worker.js';
