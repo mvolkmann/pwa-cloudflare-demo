b6cf7b126bb3aa47330394665fb2f23c6d46a0bf
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-31 19:03:21 -0600

    progress

--- public/dog-router.js
+++ public/dog-router.js
@@ -1,8 +1,15 @@
 // This file defines the routes that the service worker will handle.
+import DogController from './dog-controller';
 import {Router} from './tiny-request-router.mjs';
 
 /**
  * @typedef {{id: number, name: string, breed: string}} Dog
+ */
+
+/**
+ * This creates a Router for dog API endpoints.
+ * @param {DogController} dogController
+ * @returns {Router}
  */
 export function getRouter(dogController) {
   const router = new Router();
